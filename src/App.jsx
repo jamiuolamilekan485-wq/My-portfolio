d48c9cb3cbe5cedeb30e4d7810cad57d7219600e
jamiuolamilekan485-wq <jamiuolamilekan485@gmail.com>
2025-11-21 13:47:30 +0100

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import Projects from "./pages/Projects";
 import ProjectDetails from "./pages/projectsDetails";
 import About from "./pages/About";

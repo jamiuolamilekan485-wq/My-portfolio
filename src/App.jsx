f2ff5275fb3a94d492853467bf3a4744b52152a5
jamiuolamilekan485-wq <jamiuolamilekan485@gmail.com>
2025-11-21 12:46:43 +0100

    new

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import ProjectDetails from "./pages/projectsDetails";
+import About from "./Pages/About";
+import Contact from "./pages/Contact";
+import MainLayout from "./layouts/MainLayout";
+
+
+const App = () => {
+
+  return (
+  
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<Home />} />
+          <Route path="projects" element={<Projects />} />
+          <Route path="projects/:id" element={<ProjectDetails />} />
+          <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
+        </Route>
+      </Routes>
+   
+  );
+};
+
+export default App;
---
+++ src/data/skills.js
@@ -0,0 +1,13 @@
+const skills = [
+  "React.js",
+  "Tailwind CSS",
+  "JavaScript (ES6+)",
+  "HTML5 & CSS3",
+  "Git & GitHub",
+  "Cybersecurity Fundamentals",
+  "Ethical Hacking (CEH)",
+  "Python",
+  "Linux",
+];
+
+export default skills;
